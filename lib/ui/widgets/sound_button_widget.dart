2083452e9645b5437aac48b438defc9e0c1657d4
FutureGrit <app.futuregrit@gmail.com>
2020-10-27 00:39:29 +0530

    :bulb: Update TODO's

--- lib/ui/widgets/sound_button_widget.dart
+++ lib/ui/widgets/sound_button_widget.dart
@@ -2,16 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class SoundButtonWidget extends StatelessWidget {
-  const SoundButtonWidget({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    // TODO: Move soundEnabled variable to service "strooper_preference.dart"
     bool soundEnabled = true;
     return GestureDetector(
       onTap: () {
-        // TODO: set isSoundEnabled = !isSoundEnabled
+        // TODO: Access soundEnabled variable from "home_view_model.dart"
+        // TODO: set soundEnabled = !soundEnabled
         // TODO: Update sound icon
         print('Sound-on button taped.');
       },

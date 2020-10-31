8cf9005f33e80881e8f26f213512332b4dfbf5dc
FutureGrit <app.futuregrit@gmail.com>
2020-10-31 23:48:14 +0530

    :heavy_plus_sign: Add provider dependency and TODO's

--- lib/home/home_view_model.dart
+++ lib/home/home_view_model.dart
@@ -1,1 +1,11 @@
-// TODO: Implement [HOME] screen functionalities
+import 'package:flutter/material.dart';
+
+class HomeViewModel extends ChangeNotifier {
+  // TODO: Create method to provide high score from local DB service
+
+  // TODO: Create method to play and stop music
+
+  // TODO: Create method for navigating to Instruction screen on "i" button tapped
+
+  // TODO: Create method for navigating to PLAY screen on "Start" button tapped
+}

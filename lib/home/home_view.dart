3324d3d0a8f11be8f80c836b046b21a4dcf80360
FutureGrit <app.futuregrit@gmail.com>
2020-11-06 16:33:52 +0530

    :children_crossing: Improve user experience on back button pressed
    Implemented confirmation alert dialog to exit from game, when back button is pressed.

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -58,60 +58,84 @@ class _HomeViewState extends State<HomeView> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     // TODO: Pre load all required image assets
-    return ChangeNotifierProvider(
-      create: (context) => HomeViewModel(),
-      child: Scaffold(
-        body: Consumer<HomeViewModel>(
-          builder: (context, data, child) {
-            modelBuildContext = context;
-            return Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: appBackgroundDecoration,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AppBarWidget(),
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: ChangeNotifierProvider(
+        create: (context) => HomeViewModel(),
+        child: Scaffold(
+          body: Consumer<HomeViewModel>(
+            builder: (context, data, child) {
+              modelBuildContext = context;
+              return Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: appBackgroundDecoration,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppBarWidget(),
 
-                  // App title "STROOPER"
-                  SvgPicture.asset(
-                    'images/app_title.svg',
-                    // TODO: Move media query in [ui_helper] file
-                    width: MediaQuery.of(context).size.width / 1.3,
-                  ),
-
-                  // Start button
-                  BounceAnimation(
-                    bounceWidget: SvgPicture.asset(
-                      'images/start_button.svg',
-                      width: MediaQuery.of(context).size.width / 2.2,
+                    // App title "STROOPER"
+                    SvgPicture.asset(
+                      'images/app_title.svg',
+                      // TODO: Move media query in [ui_helper] file
+                      width: MediaQuery.of(context).size.width / 1.3,
                     ),
-                    onTap: () {
-                      Provider.of<HomeViewModel>(context, listen: false)
-                          .startGame();
-                    },
-                    // TODO: add duration parameter
-                  ),
 
-                  // Clouds and "@FutureGrit" label
-                  Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      CloudWidget(),
-                      Padding(
-                        padding: const EdgeInsets.all(paddingSmall),
-                        child: Text(
-                          '@FutureGrit',
-                          style: watermarkStyle,
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            );
-          },
+                    // Start button
+                    BounceAnimation(
+                      bounceWidget: SvgPicture.asset(
+                        'images/start_button.svg',
+                        width: MediaQuery.of(context).size.width / 2.2,
+                      ),
+                      onTap: () {
+                        Provider.of<HomeViewModel>(context, listen: false)
+                            .startGame();
+                      },
+                      // TODO: add duration parameter
+                    ),
+
+                    // Clouds and "@FutureGrit" label
+                    Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        CloudWidget(),
+                        Padding(
+                          padding: const EdgeInsets.all(paddingSmall),
+                          child: Text(
+                            '@FutureGrit',
+                            style: watermarkStyle,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
+      ),
+    );
+  }
+
+  /// Confirmation alert dialog to exit from game, when back button is pressed
+  Future<bool> _onBackPressed() {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Warning'),
+        content: Text('Do you really want to exit from STROOPER Game?'),
+        actions: [
+          FlatButton(
+            child: Text('YES'),
+            onPressed: () => Navigator.pop(context, true),
+          ),
+          FlatButton(
+            child: Text('NO'),
+            onPressed: () => Navigator.pop(context, false),
+          )
+        ],
       ),
     );
   }

85c3d6f80febcba1218c1c780632e4437b75d44a
FutureGrit <app.futuregrit@gmail.com>
2020-11-21 00:54:15 +0530

    :lipstick: Implement const for play screen
    Removed svg image background for timer widget.

--- lib/constants/ui_utils.dart
+++ lib/constants/ui_utils.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
-// ---- Margin ----
+// ---- Padding ----
 const paddingSmall = 8.0;
 const paddingNormal = 16.0;
 const paddingMedium = 20.0;
 const paddingLarge = 30.0;
 
+// ---- Margin ----
+const marginLarge = 30.0;
+
 // ---- Vertical Spacing ----
 const verticalSpacingMedium = SizedBox(width: 20);
+
+// ---- Horizontal Spacing ----
+const horizontalSpacingSmall = SizedBox(width: 16);
+
+// ---- Countdown Widget Radius ----
+const countdownWidgetRadius = 45.0; //28.0;

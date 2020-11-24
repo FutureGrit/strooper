a90ad59a758eb115451e9e6b4d385a4526a5d177
FutureGrit <app.futuregrit@gmail.com>
2020-11-24 21:26:43 +0530

    :construction: Add game color names and color code

---
+++ lib/constants/game_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const List<String> colorNames = [
+  'Red',
+  'Green',
+  'Blue',
+  'Yellow',
+  'Orange',
+  'Black'
+];
+
+const List<Color> colors = [
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.orange,
+  Colors.black
+];

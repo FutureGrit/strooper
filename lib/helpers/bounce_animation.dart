52e1bb2d2ee51257134c99b7e37a42bbd945ff55
FutureGrit <app.futuregrit@gmail.com>
2020-11-27 22:34:24 +0530

    :dizzy: Update bounce animation functionality
    1) Added [duration] to dynamically change speed of the animation.
    2) Added functionality for enable and disabling long press gesture on widget.
    3) Code refactor.

--- lib/helpers/bounce_animation.dart
+++ lib/helpers/bounce_animation.dart
@@ -1,10 +1,25 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 class BounceAnimation extends StatefulWidget {
-  BounceAnimation({@required this.bounceWidget, @required this.onTap});
+  BounceAnimation(
+      {@required this.bounceWidget,
+      @required this.onTap,
+      this.duration = 180,
+      this.disableLongPress = false})
+      : assert(bounceWidget != null),
+        assert(duration >= 1);
 
   final Widget bounceWidget;
   final Function onTap;
+
+  /// if [disableLongPress] is true then [onTap] function will be executed at the
+  /// beginning of the animation. if it's false then [onTap] function will be
+  /// executed after the animation is finished.
+  final bool disableLongPress;
+
+  /// [duration] is the length of time this animation should last.
+  final int duration;
 
   @override
   _BounceAnimationState createState() => _BounceAnimationState();
@@ -15,32 +30,32 @@ class _BounceAnimationState extends State<BounceAnimation>
   AnimationController _animationController;
   double _scale;
 
-  bool isTapped = false;
+  /// This will be used to avoid multiple tap until animation is not completed
+  bool _isTapped = false;
 
   @override
   void initState() {
     _animationController = AnimationController(
-      duration: Duration(milliseconds: 180),
-      lowerBound: 0.0,
+      duration: Duration(milliseconds: widget.duration),
       upperBound: 0.2,
+      lowerBound: 0.0,
       vsync: this,
     )
       ..addListener(() {
         setState(() {});
       })
       ..addStatusListener((status) {
-        /// Reverse animation only if button is tapped not on long press as
-        /// we are already doing reverse animation on long press end
-        if (status == AnimationStatus.completed && isTapped) {
-          _animationController.reverse();
-        }
         if (status == AnimationStatus.dismissed) {
-          isTapped = false;
-          widget.onTap();
+          _isTapped = false;
         }
       });
 
     super.initState();
+  }
+
+  Future _performAnimation() async {
+    await _animationController.forward();
+    await _animationController.reverse();
   }
 
   @override
@@ -52,28 +67,47 @@ class _BounceAnimationState extends State<BounceAnimation>
   @override
   Widget build(BuildContext context) {
     _scale = 1 - _animationController.value;
-    return GestureDetector(
-      onTap: !isTapped ? _onTap : null,
-      onLongPressStart: _onLongPressStart,
-      onLongPressEnd: _onLongPressEnd,
-      child: Transform.scale(
-        scale: _scale,
-        alignment: Alignment.center,
-        child: widget.bounceWidget,
-      ),
+    return IgnorePointer(
+      ignoring: _isTapped,
+      child: widget.disableLongPress ? _onTapWidget() : _onLongPressWidget(),
     );
   }
 
-  void _onTap() {
-    isTapped = true;
-    _animationController.forward();
+  /// [onTapWidget] function will be called if long press gesture is disabled.
+  /// This will execute [onTap] function on the beginning of the animation.
+  Widget _onTapWidget() {
+    return GestureDetector(
+      onTap: () async {
+        _isTapped = true;
+        widget.onTap();
+        await _performAnimation();
+      },
+      child: _animatedWidget,
+    );
   }
 
-  void _onLongPressStart(LongPressStartDetails details) {
-    _animationController.forward();
+  /// [onLongPressWidget] function will be called if long press gesture is enabled.
+  /// This will execute [onTap] function after the animation is completed.
+  Widget _onLongPressWidget() {
+    return GestureDetector(
+      onTap: () async {
+        _isTapped = true;
+        await _performAnimation();
+        widget.onTap();
+      },
+      onLongPressStart: (LongPressStartDetails details) =>
+          _animationController.forward(),
+      onLongPressEnd: (LongPressEndDetails details) async {
+        await _animationController.reverse();
+        widget.onTap();
+      },
+      child: _animatedWidget,
+    );
   }
 
-  void _onLongPressEnd(LongPressEndDetails details) {
-    _animationController.reverse();
-  }
+  Widget get _animatedWidget => Transform.scale(
+        scale: _scale,
+        alignment: Alignment.center,
+        child: widget.bounceWidget,
+      );
 }

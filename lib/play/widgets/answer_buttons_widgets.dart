7089b8e5c81a8640429cb38cab6b12ab27b417fa
FutureGrit <app.futuregrit@gmail.com>
2020-11-27 22:44:25 +0530

    :sparkles: Update answer buttons
    1) Added sound on button tap.
    2) Added functionality to update score if answer is correct.
    3) Code refactor.

--- lib/play/widgets/answer_buttons_widgets.dart
+++ lib/play/widgets/answer_buttons_widgets.dart
@@ -2,49 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:strooper/constants/ui_utils.dart';
+import 'package:strooper/enums/strooper_actions.dart';
 import 'package:strooper/helpers/bounce_animation.dart';
 import 'package:strooper/play/only_one_pointer_recognizer.dart';
 import 'package:strooper/play/play_view_model.dart';
+import 'package:strooper/services/sounds/sound_methods.dart';
 
 class AnswerButtonsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("------------ Rebuild Answer Button -----------------");
     return OnlyOnePointerRecognizerWidget(
       child: Container(
         margin: EdgeInsets.only(bottom: marginLarge),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            BounceAnimation(
-              onTap: () {
-                debugPrint(
-                    '##########---- Wrong Button Tapped: Button disabled -------##########');
-                Provider.of<PlayViewModel>(context, listen: false)
-                    .checkAnswer(false);
-              },
-              bounceWidget: SvgPicture.asset(
-                'images/answer_wrong.svg',
-                width: (MediaQuery.of(context).size.width / 2.1) - paddingLarge,
-              ),
+            _answerButtonType(
+              value: false,
+              imagePath: 'images/answer_wrong.svg',
+              buildContext: context,
             ),
-            BounceAnimation(
-              onTap: () {
-                debugPrint(
-                    '##########---- Right Button Tapped: Button disabled -------##########');
-                Provider.of<PlayViewModel>(context, listen: false)
-                    .checkAnswer(true);
-              },
-              bounceWidget: SvgPicture.asset(
-                'images/answer_right.svg',
-                width: (MediaQuery.of(context).size.width / 2.1) - paddingLarge,
-              ),
-            )
+            _answerButtonType(
+              value: true,
+              imagePath: 'images/answer_right.svg',
+              buildContext: context,
+            ),
           ],
         ),
       ),
     );
-    //   },
-    // );
+  }
+
+  Widget _answerButtonType({
+    @required bool value,
+    @required String imagePath,
+    @required BuildContext buildContext,
+  }) {
+    return BounceAnimation(
+      disableLongPress: true,
+      duration: 80,
+      onTap: () {
+        SoundMethods.playButtonSound(action: StrooperActions.ANSWER_BUTTON);
+        Provider.of<PlayViewModel>(buildContext, listen: false)
+            .checkAnswer(value);
+      },
+      bounceWidget: SvgPicture.asset(
+        imagePath,
+        width: (MediaQuery.of(buildContext).size.width / 2.1) - paddingLarge,
+      ),
+    );
   }
 }

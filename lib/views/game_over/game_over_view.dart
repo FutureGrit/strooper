126502fece359cf5cc54e12a2b3edcac7209e249
FutureGrit <app.futuregrit@gmail.com>
2020-12-05 23:39:57 +0530

    :construction: Add score info on game over screen

--- lib/views/game_over/game_over_view.dart
+++ lib/views/game_over/game_over_view.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:strooper/constants/shared_style.dart';
 import 'package:strooper/constants/ui_utils.dart';
@@ -16,16 +18,98 @@ class GameOverView extends StatelessWidget {
       body: Container(
           padding: EdgeInsets.symmetric(
               vertical: paddingMedium, horizontal: paddingLarge),
+          alignment: AlignmentDirectional.center,
           decoration: appBackgroundDecoration,
-          child: Center(
-              child: Container(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height / 2.1,
-            child: CustomPaint(
-              size: Size(double.infinity, 400),
-              painter: PopupBackgroundPainter(),
-            ),
-          ))),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                "images/game_over.svg",
+                width: (MediaQuery.of(context).size.width) - paddingLarge * 2,
+              ),
+              verticalSpacingMedium,
+              Stack(
+                overflow: Overflow.visible,
+                alignment: AlignmentDirectional.bottomCenter,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: countdownWidgetRadius),
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height / 2.1,
+                    child: CustomPaint(
+                      painter: PopupBackgroundPainter(
+                          backgroundColor: Color(0xffFFFAFA)),
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: paddingMedium),
+                        child: scoreDetails.isHighest
+                            ? Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'New Highest Score',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 28),
+                                  ),
+                                  Text(
+                                    '${scoreDetails.highScore}',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 40),
+                                  ),
+                                ],
+                              )
+                            : Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Your Score',
+                                    style: TextStyle(
+                                        color: Colors.red[800], fontSize: 26),
+                                  ),
+                                  Text(
+                                    '${scoreDetails.newScore}',
+                                    style: TextStyle(
+                                        color: Colors.red[800], fontSize: 36),
+                                  ),
+                                  SizedBox(height: 40),
+                                  Text(
+                                    'Highest Score',
+                                    style: TextStyle(
+                                        color: Colors.green[800], fontSize: 16),
+                                  ),
+                                  Text(
+                                    scoreDetails.highScore.toString(),
+                                    style: TextStyle(
+                                        color: Colors.green[800], fontSize: 32),
+                                  )
+                                ],
+                              ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "images/home_button.svg",
+                        width: (MediaQuery.of(context).size.width / 3) -
+                            paddingLarge,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      SvgPicture.asset(
+                        "images/restart_button.svg",
+                        width: (MediaQuery.of(context).size.width / 3) -
+                            paddingLarge,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          )),
     );
   }
 }

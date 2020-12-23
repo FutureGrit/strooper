3dc9dd33267ca65060c46f821f8f78eceaf71176
FutureGrit <app.futuregrit@gmail.com>
2020-12-23 23:53:10 +0530

    :wastebasket: Remove unused code file [ui_helper.dart]

--- lib/utils/ui_helper.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:flutter/material.dart';
-//
-// double getHeight(BuildContext context, {double divideBy = 1}) {
-//   return MediaQuery.of(context).size.height / divideBy;
-// }
-//
-// double getWidth(BuildContext context, {double divideBy = 1}) {
-//   return MediaQuery.of(context).size.width / divideBy;
-// }

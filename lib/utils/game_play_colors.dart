7c02a6e2915b034a1cb307498712894125215082
FutureGrit <app.futuregrit@gmail.com>
2020-12-27 19:48:23 +0530

    :wastebasket: Remove deprecated code for [game_play_color.dart]

--- lib/utils/game_play_colors.dart
+++ lib/utils/game_play_colors.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class GamePlayColor {
-  // TODO: Create map for colors
-
   static const List<String> colorNames = [
     'Red',
     'Green',
@@ -20,20 +18,3 @@ class GamePlayColor {
     Colors.black
   ];
 }
-// const List<String> colorNames = [
-//   'Red',
-//   'Green',
-//   'Blue',
-//   'Yellow',
-//   'Orange',
-//   'Black'
-// ];
-
-// const List<Color> colors = [
-//   Colors.red,
-//   Colors.green,
-//   Colors.blue,
-//   Colors.yellow,
-//   Colors.orange,
-//   Colors.black
-// ];
